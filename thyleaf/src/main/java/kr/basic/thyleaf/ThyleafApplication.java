aa1064d8e2cad1c106434274d038b89ab1b03da5
pyeonm30 <149234305+pyeonm30@users.noreply.github.com>
2024-03-18 13:35:39 +0900

    .
    .

---
+++ thyleaf/src/main/java/kr/basic/thyleaf/ThyleafApplication.java
@@ -0,0 +1,13 @@
+package kr.basic.thyleaf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThyleafApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ThyleafApplication.class, args);
+    }
+
+}
---
+++ thyleaf/src/test/java/kr/basic/thyleaf/ThyleafApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.basic.thyleaf;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ThyleafApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
