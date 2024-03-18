4258dd26256c0931ef47068083c55fb7d35c26b7
pyeonm30 <149234305+pyeonm30@users.noreply.github.com>
2024-03-18 13:42:03 +0900

    upload project
    .

---
+++ thyleaf/src/main/java/kr/basic/thyleaf/controller/BasicController.java
@@ -0,0 +1,145 @@
+package kr.basic.thyleaf.controller;
+
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+@Controller
+@RequestMapping("/basic")
+public class BasicController {
+
+    @GetMapping("/text-basic")
+    public String textBasic(Model model) {
+        model.addAttribute("data", "Hello Spring!");
+        return "basic/text-basic";
+    }
+
+    @GetMapping("/text-unescaped")
+    public String utextBasic(Model model) {
+        model.addAttribute("data", "Hello <b>Spring!</b>");
+        return "basic/text-unescaped";
+    }
+
+    @GetMapping("/variable")
+    public String variable(Model model) {
+        User userA = new User("userA", 10);
+        User userB = new User("userB", 20);
+
+        List<User> list = new ArrayList<>();
+        list.add(userA);
+        list.add(userB);
+
+        Map<String, User> map = new HashMap<>();
+        map.put("userA", userA);
+        map.put("userB", userB);
+
+        model.addAttribute("user", userA);
+        model.addAttribute("users", list);
+        model.addAttribute("userMap", map);
+
+        return "basic/variable";
+    }
+
+    @Data
+    @AllArgsConstructor
+    static class User {
+        private String username;
+        private int age;
+    }
+
+    @GetMapping("/basic-objects")
+    public String basicObjects(
+            HttpSession session,
+            HttpServletRequest request,
+            HttpServletResponse response,
+            Model model
+    ) {
+        session.setAttribute("sessionData", "Hello Session");
+        model.addAttribute("request", request);
+        model.addAttribute("response", response);
+        model.addAttribute("servletContext", request.getServletContext());
+        return "basic/basic-objects.html";
+    }
+
+    //-- Controller 에서 Spring Bean 으로 등록하는 방법 --//
+    @Component("/helloBean")
+    static class HelloBean {
+        public String hello(String data) {
+            return "hello" + data;
+        }
+    }
+
+    //-- 타임리프 날짜 출력시키기 --//
+    @GetMapping("/date")
+    public String date(Model model) {
+        model.addAttribute("localDateTime", LocalDateTime.now());
+        return "basic/date";
+    }
+
+    @GetMapping("link")
+    public String link(Model model) {
+        model.addAttribute("param1", "data1");
+        model.addAttribute("param2", "data2");
+        return "basic/link";
+    }
+
+    @GetMapping("/operation")
+    public String operation(Model model) {
+        model.addAttribute("nullData", null);
+        model.addAttribute("data", "Spring!");
+        return "basic/operation.html";
+    }
+
+    @GetMapping("/attribute")
+    public String attribute() {
+        return "/basci/attribute";
+    }
+
+    @GetMapping("/each")
+    public String each(Model model) {
+        addUsers(model);
+        return "basic/each";
+    }
+
+    @GetMapping("/condition")
+    public String condition(Model model) {
+        addUsers(model);
+        return "basic/condition";
+    }
+
+    @GetMapping("block")
+    public String block(Model model) {
+        addUsers(model);
+        return "basic/block";
+    }
+
+    @GetMapping("javascript")
+    public String javascript(Model model) {
+        addUsers(model);
+        model.addAttribute("user", new User("UserA", 10));
+        return "basic/javascript";
+    }
+
+    // model 을 매개변수로 받아 여러 mapping method 에서 호출해 사용될 수 있다.
+    private void addUsers(Model model) {
+        List<User> list = new ArrayList<>();
+        list.add(new User("UserA", 10));
+        list.add(new User("UserB", 20));
+        list.add(new User("UserC", 30));
+
+        model.addAttribute("users", list);
+    }
+}
